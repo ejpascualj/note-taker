ac66f788b54c03ea5e80c17efc81f4c03c17c3a2
Emilio P <ejpascualj@gmail.com>
2021-05-26 00:29:36 -0500

    modified server and added routes

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require('express');
+
+// Express Configuration
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Set up Express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// ROUTER -- points server to "route" files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// LISTENER -- starts server
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+})
