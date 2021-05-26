2aee11e0befc8823f5e86cb3ca7a690d4ba18fb1
Emilio P <ejpascualj@gmail.com>
2021-05-26 18:09:30 -0500

    deployed  heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,22 +7,27 @@ const {v4: uuidv4} = require('uuid');
 // LOAD DATA
 // Require() vs. fs.readFileSync: https://dev.to/tejesh/nodejs-read-json-file-using-require-vs-fs-module-4f94#:~:text=If%20the%20JSON%20file%20is,have%20to%20be%20re%2Dfetched.
 // In this case, 'fs.readFileSync' is better because the json file changes as new notes are added.
-let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+// let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
 
 // HELP NEEDED!!!!
 // ROUTING: https://laravel-json-api.readthedocs.io/en/latest/basics/routing/
 module.exports = (app) => {
     // API GET Request
-    app.get('/api/notes', (req, res) => res.json(data));
+    app.get('/api/notes', async(req, res) => {
+        // This is asynchronous behavior, use async-await
+        let data = await JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+        res.json(data);
+    });
 
     // API POST Request
-    app.post('/api/notes', (req, res) => {
+    app.post('/api/notes', async(req, res) => {
         // receive a new note to save on the request body and unique id
-        const newNote = request.body;
+        const newNote = req.body;
         newNote.id = uuidv4();
 
         // add new note to the db.json file
         // source??
+        let data = await JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
         data.push(newNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
 
@@ -35,20 +40,20 @@ module.exports = (app) => {
 
     // API DELETE Request (OPTIONAL)
     // https://www.codota.com/code/javascript/functions/express/Express/delete
-    app.delete('/api/notes/:id', (req, res) => {
+    app.delete('/api/notes/:id', async(req, res) => {
         // receive a query parameter containing the id of a note to delete
-        let id = req.params.id.toString();
+        let id = req.params.id;
         // In order to delete a note, you’ll need to read all notes from the db.json file
         // https://stackoverflow.com/questions/38727295/should-i-use-delete-and-findindex-or-filter-to-remove-and-item-from-an-immutable
         // https://www.codota.com/code/javascript/functions/express/Express/delete
-        const index = data.findIndex(p => p.id == id);
-
+        let data = await JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+        // const index = data.findIndex(p => p.id == id);
         // remove the note with the given id property 
-        data.splice(index,1);
+        const newData = data.filter( elements => elements.id !== id)
 
         // then rewrite the notes to the db.json file.
-        fs.writeFileSync('./db/db.json', JSON.stringify(data));
-        res.json(data);
+        fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+        res.json(newData);
     });
 };
 
--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const PORT = process.env.PORT || 8080;
 // Set up Express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+// We are using this because css and js is a static asset and we're not calling them in the routes
+app.use(express.static('public'));
 
 // ROUTER -- points server to "route" files
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
+
 
 // LISTENER -- starts server
 app.listen(PORT, () => {
