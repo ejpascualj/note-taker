f504c45fa32ec1feeb33b75411f3b6572f524ce1
Emilio P <ejpascualj@gmail.com>
2021-05-26 17:08:05 -0500

    modified htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // ROUTING
 module.exports = (app) => {
-    // HTML GET Rrequests
+    // HTML GET Requests
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
